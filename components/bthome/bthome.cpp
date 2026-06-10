77f5503ada51de8db22d600e301d957ecd40b68d
Chloe Madison-Ryan <chloe@vdbx.io>
2026-06-09 18:16:26 -0700

    Replace esp_timer_get_time with k_uptime_get

--- components/bthome/bthome.cpp
+++ components/bthome/bthome.cpp
@@ -230,7 +230,7 @@ void BTHome::setup() {
 }
 
 void BTHome::loop() {
-  uint32_t now = esp_timer_get_time() / 1000;  // Convert microseconds to milliseconds
+  uint32_t now = k_uptime_get() / 1000;  // Convert microseconds to milliseconds
 
   // Handle retransmissions
   if (this->retransmit_remaining_ > 0 && this->advertising_) {
